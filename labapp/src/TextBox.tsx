4ce17165efc3070f7442761bfff0328c7c8693fb
Caziah Mayers <caziah_mayers@brown.edu>
2022-04-02 22:43:19 -0400

    Finished lab

---
+++ labapp/src/TextBox.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function TextBox(props : any) {
+    const myHandler = (event: any) => {props.change(event.target.value)}
+    return (
+        <form>
+            <label>{props.label}
+                <input type="text" onChange={myHandler}/>
+            </label>
+        </form>
+    );
+}
+
+export default TextBox;
